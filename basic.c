ba430c4276bfca48e3708e57559e47e7b92b5caf
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2021-11-24 23:24:20 +0530

    using pointers to add numbers

--- basic.c
+++ basic.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
-{
-    int a,b;
+void main(){
+    int a,b,*p,*q;
+    p=&a;q=&b;
     printf("Enter two numbers to add:");
     scanf("%d%d",&a,&b);
-    printf("SUM = %d",a=a+b);
+    *p=*p+*q;
+    printf("SUM = %d");
     getch();
 }
