704e5d88ffb6d52e9decbc3a25e2bbf057506139
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2021-11-25 20:17:29 +0530

    added more options

--- basic.c
+++ basic.c
@@ -1,13 +1,33 @@
 #include<stdio.h>
 #include<conio.h>
 void main(){
-    int a,b,*p,*q;
-    p=&a;q=&b;
-    printf("Enter two numbers to add:");
-    scanf("%d%d",&a,&b);
-    *p=*p+*q;
-    printf("SUM = %d\n");
-    printf("Address of Sum = %u\n",p);
-    printf("addres of first digit = %u",&a);
+    int a,b,opt;
+    printf("======BASIC ARITHEMATIC OPERATION======\n");
+    printf("\t1.Addition\n");
+    printf("\t2.Substraction\n");
+    printf("\t3.multiplication\n");
+    printf("\t>");
+    scanf("%d",&opt);
+    switch (opt)
+    {
+    case 1:
+        printf("Enter the two digits for addition:");
+        scanf("%d%d",&a,&b);
+        printf("SUM = %d",a=a+b);
+        break;
+    case 2:
+        printf("Enter two digits for Substraction:");
+        scanf("%d%d",&a,&b);
+        printf("difference = %d",a=a-b);
+        break;
+    case 3:
+        printf("enter numbers for multiplication:");
+        scanf("%d%d",&a,&b);
+        printf("multiple = %d",a=a*b);
+        break;
+    default:
+        break;
+    }
+    
     getch();
 }
