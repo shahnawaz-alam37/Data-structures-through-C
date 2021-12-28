2899dcf459af01a48d4ab3bb02201ae63f3cde3f
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2021-12-28 22:14:19 +0530

    append function only(not tested)

---
+++ singlelinkedlist.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *link;
+};
+struct node *head;
+int value;
+void append(value){
+    struct node *temp;
+    temp =(struct node*)malloc(sizeof(head));
+    printf("enter data to insert into linked list:");
+    scanf("%d",&value);
+}
+int main(){
+    append();
+    
+}
