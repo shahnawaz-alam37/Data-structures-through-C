21287f84b052c70f1d9c7c2888bcda87d7338bd4
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-01-01 01:59:18 +0530

    added len function

--- singlelinkedlist.c
+++ singlelinkedlist.c
@@ -6,15 +6,67 @@ struct node
     int data;
     struct node *link;
 };
-struct node *head;
-int value;
-void append(value){
-    struct node *temp;
-    temp =(struct node*)malloc(sizeof(head));
-    printf("enter data to insert into linked list:");
-    scanf("%d",&value);
-}
+int len;
+struct node *root=NULL;
 int main(){
-    append();
-    
+    int opt;
+    for(;;)
+    {
+        system("cls");
+        printf("SINGLE LINKED LIST\n");
+        printf("1.append\n");
+        printf("2.insert at begin\n");
+        printf("3.Total length\n");
+        printf("4.EXIT\n");
+        printf("enter your choice:");
+        scanf("%d",&opt);
+        switch (opt)
+        {
+        case 1:append();
+            break;
+        case 3:len = length();
+            getch();
+            break;
+        case 4:printf("\nterminating program\n");getch();
+        exit(1);
+        default:
+            break;
+        }
+    }
+}
+void append(){
+    struct node *temp;
+    temp =(struct node*)malloc(sizeof(struct node));
+    fflush(stdin);
+    system("cls");
+    printf("enter NODE data:");
+    scanf("%d",&temp->data);
+    printf("\nData inserted\n");
+    getch();
+    temp->link=NULL;
+    if(root==NULL){
+        root=temp;
+    }
+    else{
+        struct node *p;
+        p=root;
+        while (p->link!=NULL)
+        {
+            p=p->link;
+        }
+        p->link=temp;
+    }
+}
+
+int length(){
+    struct node * temp;
+    int count=0;
+    temp=root;
+    while (temp!=NULL)
+    {
+        count++;
+        temp=temp->link;
+    }
+    printf("\nTOTAL NO. OF NODES ARE %d\n",count);
+    return count;
 }
